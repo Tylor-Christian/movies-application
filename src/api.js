d00479d932e5247246249581e429bd174bb3c2a1
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-07-20 17:10:43 -0500

    finished edit and delete functionality and added styling

--- src/api.js
+++ src/api.js
@@ -1,29 +1,41 @@
-const getMovies = () => {
-    return fetch('/api/movies')
-        .then(response => response.json())
+module.exports = {
+
+    getMovies: () => {
+        return fetch('/api/movies')
+            .then(response => response.json())
+    },
+
+    grabMovie: (id) => {
+        return fetch(`api/movies/${id}`)
+            .then(response => response.json());
+    },
+
+    addAMovie: (addMovie) => {
+        const allMovies = '/api/movies';
+        const options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(addMovie)
+        };
+        return fetch(allMovies, options)
+            .then(response => response.json());
+    },
+
+    editAMovie: (editMovie, id) => {
+        const currentMovie = `/api/movies/${id}`;
+        const options = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(editMovie)
+        };
+        return fetch(currentMovie, options)
+            .then(response => response.json());
+    },
+
+    deleteAMovie: (id) => {
+        return fetch(`/api/movies/${id}`, {
+            method: 'DELETE'
+        })
+            .then(response => response.json());
+    }
 };
-
-const addNewMovie = (addMovie) => {
-    const allMovies = '/api/movies';
-    const options = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(addMovie)
-    };
-    return fetch(allMovies, options)
-        .then(response => response.json());
-};
-
-const editMovie = (editMovie, id) => {
-    const currentMovies = `/api/movies/${id}`;
-
-    const options = {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(editMovie)
-    };
-    return fetch(currentMovies, options)
-        .then(response => response.json());
-};
-
-module.exports = {getMovies, addNewMovie, editMovie};
--- src/index.js
+++ src/index.js
@@ -2,73 +2,96 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 $("#myDiv").addClass('hide');
 
 const post = require('./api.js');
 
-$('#addMovie').click((e) => {
-    e.preventDefault();
-    const title = $('#title').val();
-    const rating = $('input[name=rating]:checked').val();
-    console.log("RATING: " + rating);
-    post.addNewMovie({title, rating});
-    setTimeout(function(){
-        moviesRefresh();
-    }, 1000);
-    $('input[type="text"], text').val('');
-    $('.form-group').find('input:radio').prop('checked', false);
-});
+// const apiLink = "http://www.omdbapi.com/?t=";
+// const apiKey = "&apikey=46042177";
+//
+// function apiMovieData(title) {
+//     $.ajax(apiLink + title + apiKey).done((data) => {
+//         console.log(data);
+//         $("").append(data.Poster);
+//     });
+// }
+// apiMovieData("deadpool");
 
-$('#editMovie').click((e) => {
-    console.log("Edit clicked");
-    console.log(e);
-    e.preventDefault();
-    let editTitle = $('#titleEdit').val();
-    console.log("EDIT TITLE: " + editTitle);
-    let editRating = $(`input[name=ratingEdit]:checked`).val();
-    console.log("EDIT RATING: " + editRating);
-    post.editMovie({title: editTitle, rating: editRating});
-    setTimeout(function(){
-        moviesRefresh();
-    }, 1000);
-    $('input[type="text"], text').val('');
-    $('.form-group').find('input:radio').prop('checked', false);
-});
-
+/**
+ * require style imports
+ */
 function moviesRefresh() {
-    getMovies().then((movies) => {
+    post.getMovies().then((movies) => {
         $("#movies").html("");
         console.log('Here are all the movies:');
         console.log(movies);
         movies.forEach(({title, rating, id}) => {
-            $("#movies").append(`<div>${title} - rating: ${rating}</div> <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#editModal">Edit</button>`);
+            $("#movies").append(`
+                <div class="card col-4 text-dark m-1">
+                    <img class="card-img-top" src="" alt="Movie Image">
+                    <div class="card-body">
+                        <h5 class="card-title"><h2>${title}</h2></h5>
+                           <p class="card-text">Movie description</p>
+                            <button id=${id} type="button" class="btn btn-sm btn-outline-primary editButton" data-toggle="modal" data-target="#editModal">Edit</button>
+                    </div>
+                </div>
+                <!--<div><h2>${title}</h2> Rating: ${rating}/5</div>-->
+        <!--<button id=${id} type="button" class="btn btn-sm btn-outline-primary editButton" data-toggle="modal" data-target="#editModal">Edit</button>-->`);
             console.log(`id#${id} - ${title} - rating: ${rating}`);
         });
         $('#loader').addClass('hide');
         $('#myDiv').removeClass('hide').addClass('show');
-    }).catch((error) => {
+    }).then(() => {
+        $('.editButton').click(function () {
+            const id = this.id;
+            post.grabMovie(id)
+                .then(movie => {
+                    $('#editMovieID').text(movie.id);
+                    $('#titleEdit').val(movie.title);
+                })
+        })
+    })
+        .catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
         console.log(error);
     });
 }
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
+moviesRefresh();
 
-getMovies().then((movies) => {
-    console.log('Here are all the movies:');
-    console.log(movies);
-    movies.forEach(({title, rating, id}) => {
-        $("#movies").append(`<div>${title} - rating: ${rating}</div> <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#editModal">Edit</button>`);
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
-    });
-    $('#loader').addClass('hide');
-    $('#myDiv').removeClass('hide').addClass('show');
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
+$('#addAMovie').click((e) => {
+    e.preventDefault();
+    const title = $('#title').val();
+    const rating = $('input[name=rating]:checked').val();
+    post.addAMovie({title, rating});
+    setTimeout(function () {
+        moviesRefresh();
+    }, 1000);
+    $('input[type="text"], text').val('');
+    $('.form-group').find('input:radio').prop('checked', false);
+});
+
+$('#editAMovie').click(function (e) {
+    e.preventDefault();
+    let id = $('#editMovieID').text();
+    let editTitle = $('#titleEdit').val();
+    let editRating = $(`input[name=ratingEdit]:checked`).val();
+    post.editAMovie({title: editTitle, rating: editRating}, id);
+    setTimeout(function () {
+        moviesRefresh();
+    }, 1000);
+    $('input[type="text"], text').val('');
+    $('.form-group').find('input:radio').prop('checked', false);
+});
+
+$('#deleteAMovie').click(() => {
+    let id = $('#editMovieID').text();
+    console.log(id);
+    post.deleteAMovie(id);
+    setTimeout(function () {
+        moviesRefresh();
+    }, 1000);
 });
